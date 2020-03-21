4bba3a211b2e23de8213c0cfb27804b3d9ad77d7
yamotonalds <yamotonalds@gmail.com>
2020-03-21 13:42:27 +0900

    rails g scaffold product_id:references item name:string amount:integer and rails db:migrate

---
+++ app/views/items/_item.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! item, :id, :product_id_id, :name, :amount, :created_at, :updated_at
+json.url item_url(item, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_041831) do
+ActiveRecord::Schema.define(version: 2020_03_21_043931) do
+
+  create_table "items", force: :cascade do |t|
+    t.integer "product_id_id", null: false
+    t.string "name"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id_id"], name: "index_items_on_product_id_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_03_21_041831) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "product_ids"
 end
