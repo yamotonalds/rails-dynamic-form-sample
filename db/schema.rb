d1077eaef6a2691541282fba70ae54fb7f1bdbc5
yamotonalds <yamotonalds@gmail.com>
2020-03-21 14:09:22 +0900

    rails g scaffold product:references item name:string amount:integer and rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_041831) do
+ActiveRecord::Schema.define(version: 2020_03_21_050812) do
+
+  create_table "items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.string "name"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_03_21_041831) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "products"
 end
