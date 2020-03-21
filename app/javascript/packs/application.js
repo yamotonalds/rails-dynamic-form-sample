9f785b0de5b4a184159444bb0a8dd0949928741a
yamotonalds <yamotonalds@gmail.com>
2020-03-21 19:42:24 +0900

    setup cocoon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("cocoon/app/assets/javascripts/cocoon")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
