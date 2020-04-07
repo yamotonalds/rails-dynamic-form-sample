9e98e1cdfd68ec36e63557b2cc6e2fe93162a526
yamotonalds <yamotonalds@gmail.com>
2020-04-07 16:16:32 +0900

    bundle exec rubocop --auto-correct
    Layout/EmptyLinesAroundAccessModifier

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,6 +62,7 @@ class ProductsController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
